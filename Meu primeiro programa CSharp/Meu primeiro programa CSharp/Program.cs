b4974a88f328357e92b1ead542ddd9029a5d04a1
ducrz <ducatolico@gmail.com>
2021-09-21 16:53:00 -0300

    Fim Seção 2: Preparação do ambiente e introdução à linguagem C# e a plataforma .NET

---
+++ Meu primeiro programa CSharp/Meu primeiro programa CSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meu_primeiro_programa_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá C#!");
+            Console.WriteLine();
+            Console.WriteLine("Pressione uma tecla para Continuar!");
+            Console.ReadKey();
+        }
+    }
+}
