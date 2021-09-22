c79d0f5377ad48d256ade065c4dddf49c2a91174
ducrz <ducatolico@gmail.com>
2021-09-22 14:09:06 -0300

    Seção 03: C# Básico. Estruturas condicionais simples.

--- MeuPrimeiroProjeto/MeuPrimeiroProjeto/Program.cs
+++ MeuPrimeiroProjeto/MeuPrimeiroProjeto/Program.cs
@@ -11,25 +11,82 @@ namespace MeuPrimeiroProjeto
         {
             //Calculadora Simples
 
-            Console.Write("Digite o primeiro valor: ");
-            string valor1 = Console.ReadLine();
+            //Console.Write("Digite o primeiro valor: ");
+            //string valor1 = Console.ReadLine();
+
+            //while (Convert.ToInt32(valor1) == 0)
+            //{
+            //    Console.WriteLine("Valor Inválido. Digite o Primeiro Valor Novamente: ");
+            //    valor1 = Console.ReadLine();
+
+            //}
+
+            //Console.WriteLine();
+
+            //Console.Write("Insira o segundo valor: ");
+            //string valor2 = Console.ReadLine();
+
+
+            //while (Convert.ToInt32(valor2) == 0)
+            //{
+            //    Console.WriteLine("Valor Inválido. Digite o Segundo Valor Novamente: ");
+            //    valor2 = Console.ReadLine();
+
+            //}
+
+            //Console.WriteLine();
+
+            //Console.Write("Insira o Operador (+),(-),(*) ou (/): ");
+            //string operador = Console.ReadLine();
+            //decimal resultado = 0;
+
+            //if(operador == "+")
+            //    resultado = Convert.ToInt32(valor1) + Convert.ToInt32(valor2);
+            //if (operador == "-")
+            //    resultado = Convert.ToInt32(valor1) - Convert.ToInt32(valor2);
+            //if (operador == "*")
+            //    resultado = Convert.ToInt32(valor1) * Convert.ToInt32(valor2);
+            //if (operador == "/")
+            //    resultado = Convert.ToInt32(valor1) / Convert.ToInt32(valor2);
+
+            //Console.WriteLine("O Resultado é: "+resultado);
+
+            //Console.ReadKey();
+
+
+            //Sistema de Login
+
+            string usuarioSistema = "usuario";
+            string senhaSistema = "123";
+
+
+            Console.Write("Digite o nome do Usuário: ");
+            string usuarioInserido = Console.ReadLine();
 
             Console.WriteLine();
 
-            Console.Write("Insira o segundo valor: ");
-            string valor2 = Console.ReadLine();
 
-            Boolean resultado = valor1 == valor2;
+            Console.Write("Digite a senha: ");
+            string senhaInserida = Console.ReadLine();
 
-            Console.WriteLine("Resultado é igual: "+resultado);
             Console.WriteLine();
 
 
-            Boolean comparacaoMaiorQue = Convert.ToInt16(valor1) > Convert.ToInt16(valor2);
+            if (usuarioInserido != usuarioSistema || senhaInserida != senhaSistema){
+                Console.ForegroundColor = ConsoleColor.Red;
 
-            Console.WriteLine("Resultado é valor 1 maior que valor 2 : "+comparacaoMaiorQue);
+                Console.WriteLine("Usuario ou Senha Incorretos! ");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Bem-vindo ao Sistema!");
+            }           
 
             Console.ReadKey();
+
+
+
 
 
         }
