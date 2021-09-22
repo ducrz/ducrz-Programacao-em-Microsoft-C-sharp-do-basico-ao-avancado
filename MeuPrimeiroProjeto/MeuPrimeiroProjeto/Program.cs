ae205d28174421da519197d23e6f868b8d2ae5e6
ducrz <ducatolico@gmail.com>
2021-09-22 10:38:07 -0300

    Seção 03: C# Básico. Elementos da Linguagem C#

---
+++ MeuPrimeiroProjeto/MeuPrimeiroProjeto/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+
+
+namespace MeuPrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)            
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Olá C#");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Olá usando WriteLine");
+            Console.ReadKey();
+
+        }
+    }
+}
